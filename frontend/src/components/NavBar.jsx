54fa6a968645998825fba73cc91d9a49027b0a07
Erljan Rodrigo <erljan0y@gmail.com>
2024-08-28 15:30:58 -0400

    navbar added

---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,28 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Link } from 'react-router-dom';
+
+
+export function NavBar() {
+  return (
+    <Navbar expand="lg" className="navbar" data-bs-theme="dark">
+      <Container>
+        <Navbar.Brand as={Link} to="/">Event Master</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/myevents">My events</Nav.Link>
+            <Nav.Link as={Link} to="/profile">Profile</Nav.Link>
+            <NavDropdown title="Logout" id="basic-nav-dropdown">
+              <NavDropdown.Item as={Link} to="/logout">Logout</NavDropdown.Item>
+              
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
