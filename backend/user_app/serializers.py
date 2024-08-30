421673faa607acfefe3107945a2275f22b90288a
Erljan Rodrigo <erljan0y@gmail.com>
2024-08-30 17:24:44 -0400

    added the profile info

--- backend/user_app/serializers.py
+++ backend/user_app/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from .models import UserProfile
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -10,3 +11,10 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         data = User.objects.create_user(**validated_data)
         return data
+    
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+    class Meta:
+        model = UserProfile
+        fields = ['user', 'bio']
