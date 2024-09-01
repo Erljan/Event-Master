dacb1e54a83a6b24579c1029fc8999e08c832195
Erljan Rodrigo <erljan0y@gmail.com>
2024-09-01 19:50:51 -0400

    backend for profile update

--- backend/user_app/views.py
+++ backend/user_app/views.py
@@ -5,6 +5,9 @@ from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from .models import UserProfile
 from rest_framework.exceptions import NotFound
+from rest_framework.response import Response
+from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED, HTTP_404_NOT_FOUND, HTTP_400_BAD_REQUEST
+
 
 
 # class CreateUserViews(generics.CreateAPIView):
@@ -35,8 +38,31 @@ class GetUserInfo(generics.RetrieveAPIView):
 
 class UpdateProfileInfo(generics.UpdateAPIView):
     serializer_class = UserProfileSerializer
-    queryset = UserProfile.objects.all()
+    permission_classes = [IsAuthenticated]
 
-    def perform_update(self, serializer):
+    def get_object(self):
+        # retrieve the userprofile
+        user_profile = UserProfile.objects.get(user=self.request.user)
+        return user_profile
+    
+    def put(self, request, *args, **kwargs):
+        profile = self.get_object()
+        user_data = request.data.pop('user', {})
+
+        # Update User fields if provided
+        user = profile.user
+        if 'first_name' in user_data:
+            user.first_name = user_data['first_name']
+        if 'last_name' in user_data:
+            user.last_name = user_data['last_name']
+        if 'email' in user_data:
+            user.email = user_data['email']
+        user.save()
+
+        # Update the UserProfile fields
+        serializer = self.get_serializer(profile, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
         
-        serializer.save(user=self.request.user)
