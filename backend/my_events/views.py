a4352160d75e66786a46ab06349b633ac53b7702
Erljan Rodrigo <erljan0y@gmail.com>
2024-09-09 17:18:28 -0400

    added myeven backend

---
+++ backend/my_events/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render
+from django.contrib.auth.models import User
+from .serializers import MyEventSerializer
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from .models import MyEvents
+from rest_framework.exceptions import NotFound
+from rest_framework.response import Response
+from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED, HTTP_404_NOT_FOUND, HTTP_400_BAD_REQUEST
+
+
+class AddToMyEvents(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = MyEventSerializer
+
+    def get_queryset(self):
+        return MyEvents.objects.filter(owner=self.request.user).order_by("added_at")
+    
+    def perform_create(self, serializer):
+        event_id = self.request.data.get('eventId')
+
+        if MyEvents.objects.filter(eventId=event_id, owner=self.request.user).exists():
+            return Response({"error": "Event already saved"}, status=HTTP_400_BAD_REQUEST)
+
+        serializer.save(owner=self.request.user)
+
+    def post(self, request, *args, **kwargs):
+        event_id = request.data.get("eventId")
+
+        if not event_id:
+            return Response({"error": "Event ID is required"}, status=HTTP_400_BAD_REQUEST)
+        
+        return self.create(request, *args, **kwargs)
+    
+class MyEventList(generics.ListAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = MyEventSerializer
+
+    def get_queryset(self):
+        # Return the events saved by the authenticated user
+        return MyEvents.objects.filter(owner=self.request.user)
