9e5f884fc51298360e8d8bb36f65d7f05c56dab3
garrett <garrettgt100@gmail.com>
2024-09-10 22:38:28 -0700

    gh links added to footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,11 +1,40 @@
 import React from "react";
 import "../styles/Footer.css";
+import { FaGithub } from "react-icons/fa";
 import logo from "../images/logo.png";
 
 export default function Footer() {
   return (
     <footer className="footer">
-      <p className="text">Created by Mickey, Erljan, and Garrett</p>
+      <p className="text">
+        Created by Mickey Gardner, Erljan Rodrigo, and Garrett Gilbert.
+      </p>
+      <a
+        className="github-link"
+        target="_blank"
+        href="https://github.com/mickeygard"
+      >
+        <FaGithub className="gh-logo" />
+        mickeygard
+      </a>{" "}
+      <a
+        className="github-link"
+        target="_blank"
+        href="https://github.com/Erljan"
+      >
+        <FaGithub className="gh-logo" />
+        Erljan
+      </a>{" "}
+      <a
+        className="github-link"
+        target="_blank"
+        href="https://github.com/garrettgt"
+      >
+        <FaGithub className="gh-logo" />
+        garrettgt
+      </a>
+      <br></br>
+      <br></br>
       <img
         className="logo"
         src={logo}
